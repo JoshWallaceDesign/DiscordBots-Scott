1a13e35577801e8d05e3961b454973e1ac53459d
Josh <joshwallacemp@gmail.com>
2020-02-06 21:53:34 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -314,11 +314,9 @@ client.on("message", message => {
     if(message.content.toLowerCase() == "!timerstop")
     {
         clearInterval(clock);
-        message.channel.send({embed :{
-            color: 15158332,
-            title: "Timer Stopped"
-        }})
-    }
+        }
+    message.channel.send("Timer Stopped");
+
 });
     
 
@@ -346,7 +344,7 @@ client.on('message', message => {
     if(message.content.toLowerCase() === '!' + 'help') {
         message.channel.send({embed: {
             color: 3447003,
-            description: "Here are the commands:\n```Coin Flip : !flip \nTimer : !timer 180, 90, 60, 45 or 30 \n !timerstop : Stops Timer```"
+            description: "Here are the commands:\n```Coin Flip : !flip \nTimer : !timer 180, 90, 60, 45 or 30 \nStop Timer : !timerstop```"
         }});
         
     }
